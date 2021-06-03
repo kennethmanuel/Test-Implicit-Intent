1ab300c4fc4b5590fe4d8c7dca8d4ed260e41dcd
Kenneth Manuel <kennethlieyanto99@gmail.com>
2021-06-03 09:50:19 +0700

    W13

--- app/src/main/java/com/kennethmanuel/a160419041_week12/MainActivity.kt
+++ app/src/main/java/com/kennethmanuel/a160419041_week12/MainActivity.kt
@@ -2,21 +2,55 @@ package com.kennethmanuel.a160419041_week12
 
 import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_SELECT_CONTACT = 1
     val REQUEST_SELECT_TEMPLATE = 2
+    val REQUEST_IMAGE_CAMERA = 3
+
+    private fun takePicture() {
+        val intent = Intent().apply {
+            action = MediaStore.ACTION_IMAGE_CAPTURE
+        }
+        startActivityForResult(intent, REQUEST_IMAGE_CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_IMAGE_CAMERA -> {
+                if(grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED)
+                    takePicture()
+                else
+                    Toast.makeText(this, "Anda harus memberikan izin untuk mengakses kamera.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         buttonSend.setOnClickListener {
             // Buat send intent
@@ -42,6 +76,14 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MessageTemplateActivity::class.java)
             startActivityForResult(intent, REQUEST_SELECT_TEMPLATE)
         }
+        fabFoto.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), REQUEST_IMAGE_CAMERA)
+            } else {
+                takePicture()
+            }
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -67,6 +109,12 @@ class MainActivity : AppCompatActivity() {
                 REQUEST_SELECT_TEMPLATE -> {
                     val pesan = data?.getStringExtra(MessageTemplateActivity.EXTRA_TEMPLATE)
                     editPesan.setText(pesan)
+                }
+                REQUEST_IMAGE_CAMERA -> {
+                    data?.extras?.let {
+                        val imageBitmap = it.get("data") as Bitmap
+                        imagePhoto.setImageBitmap(imageBitmap)
+                    }
                 }
             }
         }
